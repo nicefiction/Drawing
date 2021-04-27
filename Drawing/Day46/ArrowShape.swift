f140f890f4cd36f39426950cb3d7026282ef9700
Olivier Van hamme <nicefiction@gmail.com>
2021-04-27 07:21:59 +0200

    Makes the line thickness of the arrow shape animatable .

--- Drawing/Day46/ArrowShape.swift
+++ Drawing/Day46/ArrowShape.swift
@@ -28,26 +28,51 @@ struct ArrowShape: View {
      // /////////////////
     //  MARK: PROPERTIES
     
+    let goldenRatio: CGFloat = 1.618
+
+    
     
      // //////////////////////////
     //  MARK: COMPUTED PROPERTIES
     
     var body: some View {
-        let goldenRatio: CGFloat = 1.618
+
+        // SOLUTION 1 :
+//        ZStack {
+//            TriangleShape()
+//                .stroke(lineWidth s: 8)
+//                .frame(width : 150 * goldenRatio , height : 100)
+//            VStack {
+//                Rectangle()
+//                    .frame(width : 150 * goldenRatio , height : 335)
+//                    .foregroundColor(Color.blue.opacity(0.00))
+//                Rectangle()
+//                    .frame(width : 150 , height : 150 * goldenRatio)
+//                    .foregroundColor(Color.blue.opacity(0.00))
+//                    .overlay(Rectangle().stroke(lineWidth : 8))
+//            }
+//        }
         
-        ZStack {
+        // SOLUTION 2
+//        VStack(spacing : 0) {
+//            Triangle()
+//                .stroke(lineWidth : 8)
+//                .frame(width : 150 * goldenRatio , height : 100)
+//            Rectangle()
+//                .frame(width : 150 , height : 150 * goldenRatio)
+//                .foregroundColor(Color.blue.opacity(0.00))
+//                .overlay(Rectangle().stroke(lineWidth : 8))
+//        }
+        
+        // SOLUTION 3 :
+        VStack(spacing : 0) {
             TriangleShape()
-                .stroke(lineWidth: 8)
+                .stroke(lineWidth : 8)
                 .frame(width : 150 * goldenRatio , height : 100)
-            VStack {
-                Rectangle()
-                    .frame(width : 150 * goldenRatio , height : 335)
-                    .foregroundColor(Color.blue.opacity(0.00))
-                Rectangle()
-                    .frame(width : 150 , height : 150 * goldenRatio)
-                    .foregroundColor(Color.blue.opacity(0.00))
-                    .overlay(Rectangle().stroke(lineWidth: 8))
-            }
+            Rectangle()
+                .frame(width : 150 , height : 150 * goldenRatio)
+                .foregroundColor(Color.blue.opacity(0.00))
+                .overlay(Rectangle().stroke(lineWidth : 8))
         }
     }
 }
---
+++ Drawing/Day46/ArrowShapeAnimatable.swift
@@ -0,0 +1,107 @@
+// MARK: ArrowShapeAnimatable.swift
+
+import SwiftUI
+
+
+
+struct TriangleShape2: Shape {
+    
+     // /////////////////
+    //  MARK: PROPERTIES
+    
+    var lineThicknessAmount: CGFloat
+    
+    
+    
+     // //////////////////////////
+    //  MARK: COMPUTED PROPERTIES
+    
+    var animatableShape: CGFloat {
+        
+        get {
+            lineThicknessAmount
+        }
+        
+        set {
+            lineThicknessAmount = newValue
+        }
+    }
+    
+     // /////////////////
+    //  PROTOCOL METHODS
+    
+    func path(in rect: CGRect)
+    -> Path {
+        
+        var path: Path = Path()
+        
+        path.move(to : CGPoint(x : rect.midX , y : rect.minY))
+        path.addLine(to : CGPoint(x : rect.minX , y : rect.maxY))
+        path.addLine(to : CGPoint(x : rect.maxX , y : rect.maxY))
+        path.addLine(to : CGPoint(x : rect.midX , y : rect.minY))
+        path.addLine(to : CGPoint(x : rect.minX , y : rect.maxY))
+        
+        return path
+    }
+}
+
+
+
+struct ArrowShapeAnimatable: View {
+    
+     // /////////////////
+    //  MARK: PROPERTIES
+    
+    let goldenRatio: CGFloat = 1.618
+    
+    
+    
+     // ///////////////////////
+    // MARK: PROPERTY WRAPPERS
+    
+    @State private var lineThicknessAmount: CGFloat = 8
+    
+    
+    
+    // //////////////////////////
+   //  MARK: COMPUTED PROPERTIES
+    
+    var body: some View {
+        
+        VStack(spacing : 0) {
+            TriangleShape()
+                .stroke(lineWidth : lineThicknessAmount)
+                .frame(width : 150 * goldenRatio ,
+                       height : 100)
+            Rectangle()
+                .frame(width : 150 ,
+                       height : 150 * goldenRatio)
+                .foregroundColor(Color
+                                    .blue
+                                    .opacity(0.00))
+                .overlay(
+                    Rectangle()
+                        .stroke(lineWidth : lineThicknessAmount))
+        }
+        .onTapGesture {
+            withAnimation {
+                lineThicknessAmount = CGFloat(Int.random(in: 1...10))
+            }
+        }
+    }
+}
+
+
+
+
+
+ // //////////////
+// MARK: PREVIEWS
+
+struct ArrowShapeAnimatable_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        
+        ArrowShapeAnimatable().previewDevice("iPhone 12 Pro")
+    }
+}
